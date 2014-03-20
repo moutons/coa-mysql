70cf0fc86c3cb765357c7ec3bd5372a0b7ca3321
Jerald Sheets <questy@gmail.com>
2014-03-20 17:20:01 -0500

    reverted to older classloading method

--- manifests/init.pp
+++ manifests/init.pp
@@ -1,6 +1,5 @@
 # CoA MySQL Module
 class coa-mysql {
-  class { '::coa-mysql::config': } ->
-  class { '::coa-mysql::service': } ->
-  Class['coa-mysql']
+  include coa-mysql::config
+  include coa-mysql::service
 }
