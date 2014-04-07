0986f730d54e725c6212532b46225ebf7926115b
Shaun Mouton <shaun.mouton@austintexas.gov>
2014-04-07 14:22:08 -0500

    adding mysql user

--- manifests/init.pp
+++ manifests/init.pp
@@ -7,12 +7,19 @@ class coa-mysql {
 class { '::mysql::server':
   root_password => 'puppet',
   $users = {
+    'drupal2@*'     => {
+      ensure        => 'present',
+      password_hash => '*BEC8AE1C6D76DCC3A13D1C04DB54D3BFA6D979EB',
+    },
+  },
+  $grants = {
     'drupal2@*/drupal1.*' => {
       ensure              => 'present',
       options             => ['GRANT'],
       privileges          => ['ALL'],
       table               => 'drupal1.*',
       user                => 'drupal2@*',
+
     },
   },
   $databases = {
