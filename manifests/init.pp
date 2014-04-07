bb77debdbe34ca7b70d7f21ec037e8b5be61e72e
Shaun Mouton <shaun.mouton@austintexas.gov>
2014-04-07 13:58:33 -0500

    polluting w puppetlabs/mysql

--- manifests/init.pp
+++ manifests/init.pp
@@ -3,3 +3,22 @@ class coa-mysql {
   include coa-mysql::config
   include coa-mysql::service
 }
+
+class { '::mysql::server':
+  root_password => 'puppet',
+  $users = {
+    'drupal2@*/drupal1.*' => {
+      ensure              => 'present',
+      options             => ['GRANT'],
+      privileges          => ['ALL'],
+      table               => 'drupal1.*',
+      user                => 'drupal2@*',
+    },
+  },
+  $databases = {
+    'drupal1' => {
+      ensure  => 'present',
+      charset => 'utf8',
+    },
+  },
+}
